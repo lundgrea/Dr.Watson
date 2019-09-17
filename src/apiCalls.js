1ead1c8d335921f0b8b1e884c472d3e757117e82
Alyssa Lundgren <lundgrea@gmail.com>
2019-09-17 17:47:35 -0600

    Implement postMessage fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -19,7 +19,23 @@ export const startConversation = async feeling => {
 }
 
 export const postMessage = async newMessage => {
+  const url = 'https://drwatson-api.herokuapp.com/api/message'
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ newMessage })
+  };
 
+  const response = await fetch(url, options);
+  if(!response.ok) {
+    throw Error('Dr. Watson is not in the office. Please try again later.')
+  }
+
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
 
 export const endConversation = async () => {
